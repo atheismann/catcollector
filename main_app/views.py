eeab5408330d56e58e3000f7a7243bd5c3cdb3a1
Andrew Theismann <andrew@theismannconsulting.com>
2019-09-09 17:15:16 -0500

    working day 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,19 @@
+
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from .models import Cat
+
+# View functions
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
 
-# Create your views here.
+def cats_index(request):
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', { 'cats': cats })
 
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', { 'cat': cat })
