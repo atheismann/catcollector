0e9ffcae5e9e93f70574569ef9bc5f2433ec44d4
Andrew Theismann <andrew@theismannconsulting.com>
2019-09-10 16:22:12 -0500

    add feeding

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
-# Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Cat(models.Model):
   name = models.CharField(max_length=100)
@@ -14,3 +19,23 @@ class Cat(models.Model):
   
   def get_absolute_url(self):
     return reverse('detail', kwargs={'cat_id': self.id})
+
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0],
+  )
+
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
+
