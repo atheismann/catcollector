4957659a782b8427357d2cef7d163e5422c1435b
Andrew Theismann <andrew@theismannconsulting.com>
2019-09-12 16:22:50 -0500

    add auth

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,15 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Feeding
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class FeedingForm(ModelForm):
   class Meta:
     model = Feeding
     fields = ['date', 'meal']
+
+class SignupForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
